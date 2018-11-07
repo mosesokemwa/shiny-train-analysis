93fa7ab1b408cc870cce07e0d35405fc968c836e
xv <xavierkibet@gmail.com>
2018-11-07 18:43:48 +0300

    Update provider package for provider detection

--- jobs/providers/__init__.py
+++ jobs/providers/__init__.py
@@ -1,12 +1,27 @@
 from .AbstractProvider import AbstractProvider
 from .IHubProvider import IHubProvider
 from .BrighterMondayProvider import BrighterMondayProvider
+from .CareerPointProvider import CareerPointProvider
+from .FuzuProvider import FuzuProvider
+from .PigiaMeProvider import PigiaMeProvider
+from .JobWebKenyaProvider import JobWebKenyaProvider
+import json
 
 provider_abstract = AbstractProvider.__subclasses__()
-
 providers = {}
+num = 0
+
+
+def callback(dict_object):
+    global num
+    num += 1
+    print(num)
+
 
 for provider_main in provider_abstract:
     for provider in provider_main.__subclasses__():
-        providers[provider.host] = provider()
-
+        host = provider.__dict__.get("host")
+        hosts = [host] if type(host) is str else host
+        for host in hosts:
+            providers[host] = provider()
+            setattr(providers[host], 'post_process', callback)
