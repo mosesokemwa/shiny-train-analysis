0385f80081e6bbac8d0eb86cd4587f37bb4edc29
xv <xavierkibet@gmail.com>
2018-11-07 18:46:54 +0300

    extend brighter monday handler to multiple domains

--- jobs/providers/BrighterMondayProvider.py
+++ jobs/providers/BrighterMondayProvider.py
@@ -4,14 +4,17 @@ from .AbstractTokenProvider import AbstractTokenProvider
 
 class BrighterMondayProvider(AbstractTokenProvider):
     pagination = 'page'
-    host = 'brightermonday.co.ke'
+    host = ['brightermonday.co.ke', 'brightermonday.co.ug', 'brightermonday.co.tz']
 
     def fetch(self, entry_url: str) -> JobsList:
         self.jobs = JobsList()
         page_buffer = []
 
         for job_link in self.get_jobs_list(entry_url):
-            page_buffer.append(self.get_job(job_link))
+            try:
+                page_buffer.append(self.get_job(job_link))
+            except Exception as e:
+                print("Error adding job at %s %s" % (job_link, e))
 
         page = 2
         while page_buffer:
@@ -19,7 +22,10 @@ class BrighterMondayProvider(AbstractTokenProvider):
             page_buffer = []
             loop_url = f'{entry_url}?{self.pagination}={page}'
             for job_link in self.get_jobs_list(loop_url):
-                page_buffer.append(self.get_job(job_link))
+                try:
+                    page_buffer.append(self.get_job(job_link))
+                except Exception as e:
+                    print("Error adding job at %s %s" % (job_link, e))
             page += 1
 
         return self.jobs
