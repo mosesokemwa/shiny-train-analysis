f46efaf3ffcd6654f685ce52fd5a703548beed15
Moses Okemwa <okemwamoses@gmail.com>
2018-11-07 17:51:51 +0300

    configurations

---
+++ jobs/defaults.py
@@ -0,0 +1,5 @@
+from jobs.handlers.postgres import postgres_storage_handler
+from jobs.handlers.mongodb import mongodb_storage_handler
+
+
+StorageHandler = mongodb_storage_handler.MongoDbStorageHandler
