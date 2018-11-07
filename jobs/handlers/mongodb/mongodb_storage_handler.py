2424c495d93c0f7196c35b865a851dd9f69baa90
Moses Okemwa <okemwamoses@gmail.com>
2018-11-07 18:00:15 +0300

    mongodb storage handler init

---
+++ jobs/handlers/mongodb/mongodb_storage_handler.py
@@ -0,0 +1,25 @@
+from typing import List,Dict
+from pymongo import MongoClient
+from jobs.handlers.abstract_storage_handler import AbstractStorageInterface
+from jobs.handlers.mongodb.config import MongoConfig
+
+class MongoDbStorageHandler(AbstractStorageInterface):
+
+    def __init__(self, mongoconf=MongoConfig(), *args, **kwargs):
+        self.client=MongoClient(mongoconf.get_database_uri())
+        self.database=self.client["{}".format(mongoconf.DATABASE_NAME)]
+
+    def write(self,data:List[Dict]) -> bool:
+        try:    
+            mongo_col=self.database["jobs_records"]
+            d={"student":"ty ty"}
+            ids=mongo_col.insert_many(data)
+            print(ids.inserted_ids)
+            return True
+
+        except Exception as e:
+            print(e)
+            return False
+
+    def read(self):
+        pass
