9aa2b1695640b2c614dce0dbfa2a72168b69ee72
Moses Okemwa <okemwamoses@gmail.com>
2018-11-07 17:53:09 +0300

    entities& services

---
+++ jobs/entities.py
@@ -0,0 +1,7 @@
+class Job:
+    def __init__(self, **kwargs):
+        pass
+
+
+class JobsList(list):
+    pass
---
+++ jobs/services.py
@@ -0,0 +1,27 @@
+from .entities import JobsList
+from urllib.parse import urlparse
+from .providers import providers
+
+class JobsService:
+    def __init__(self):
+        self.providers_service = ProvidersService()
+
+    def fetch_provider(self, job: str) -> JobsList:
+        return self.providers_service.fetch_provider(job)
+
+    def fetch_list(self, providers: list) -> JobsList:
+        job_list = JobsList()
+        for provider in providers:
+            job_list.extend(self.fetch_provider(provider))
+
+        return job_list
+
+
+class ProvidersService:
+
+    def fetch_provider(self, provider: str) -> JobsList:
+        parsed = urlparse(provider)
+        host = parsed.netloc
+        jobs = providers[host].fetch(provider)
+        return jobs
+
