2833e9d4652943ca4c22cdbfb3afe95e480910b5
Moses Okemwa <okemwamoses@gmail.com>
2018-11-08 23:21:50 +0300

    edited main script
    added fetching data from mongo
    writing data to csv file

--- jobs.py
+++ jobs.py
@@ -1,21 +1,27 @@
 from jobs.services import JobsService
 from jobs.defaults import StorageHandler
+from jobs.handlers.csv_storage_handler import CsvStorageHandler
 
 provider_list = [
-    "https://www.fuzu.com/categories/it-software"
+    "https://www.fuzu.com/categories/it-software",
     "https://www.brightermonday.co.ug/jobs/technology/",
     "https://www.brightermonday.co.ke/jobs/technology/",
     "https://www.brightermonday.co.tz/jobs/technology/",
     "https://www.careerpointkenya.co.ke/category/ict-jobs-in-kenya/",
     "https://www.pigiame.co.ke/it-telecoms-jobs",
     "https://jobwebkenya.com/job-category/ittelecom-jobs-in-kenya-2013/",
-    "https://ihub.co.ke/jobs"
+    "https://ihub.co.ke/jobs",
 ]
 
+# jobs_service = JobsService()
+# jobs = jobs_service.fetch_list(provider_list)
 
-jobs_service = JobsService()
-jobs = jobs_service.fetch_list(provider_list)
-print("Fetched %s jobs."%len(jobs))
+# for job_list in jobs:
+#     print("Fetched %s jobs."%len(job_list))
+#     StorageHandler().write(job_list)
+#     print("Stored %s jobs."%len(job_list))
 
-StorageHandler().write(jobs)
-print("Stored %s jobs."%len(jobs))
+data=StorageHandler().fetch_all()
+
+CsvStorageHandler().write(data)
+
